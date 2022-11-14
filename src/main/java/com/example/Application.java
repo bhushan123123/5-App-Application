4941982eeaa086bd34e1ff6f5109a12fd91ccaee
Bhushan <bhushansamadur3@gmail.com>
2022-11-14 16:06:30 +0530

    App Loadd

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=1000;i++) {
+			
+		}
+		SpringApplication.run(Application.class, args);
+	}
+
+}
